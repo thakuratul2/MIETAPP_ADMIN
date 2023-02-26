4bcec4b0971ae4080529b3103a0fb1c60142a7d7
thakuratul2 <pratapsinghatul111@gmail.com>
2023-02-26 11:56:27 +0530

    Fix

--- admin/add-teacher.php
+++ admin/add-teacher.php
@@ -9,70 +9,42 @@ if (strlen($_SESSION['sturecmsaid']==0)) {
   {
  $stuname=$_POST['stuname'];
  $stuemail=$_POST['stuemail'];
- $stuclass=$_POST['stuclass'];
+ 
  $gender=$_POST['gender'];
- $dob=$_POST['dob'];
- $stuid=$_POST['stuid'];
- $fname=$_POST['fname'];
- $mname=$_POST['mname'];
+
  $connum=$_POST['connum'];
- $altconnum=$_POST['altconnum'];
- $address=$_POST['address'];
  $uname=$_POST['uname'];
  $password=md5($_POST['password']);
- $image=$_FILES["image"]["name"];
+ 
  $ret="select UserName from tblstudent where UserName=:uname || StuID=:stuid";
  $query= $dbh -> prepare($ret);
 $query->bindParam(':uname',$uname,PDO::PARAM_STR);
 $query->bindParam(':stuid',$stuid,PDO::PARAM_STR);
 $query-> execute();
      $results = $query -> fetchAll(PDO::FETCH_OBJ);
-if($query -> rowCount() == 0)
-{
-$extension = substr($image,strlen($image)-4,strlen($image));
-$allowed_extensions = array(".jpg","jpeg",".png",".gif");
-if(!in_array($extension,$allowed_extensions))
-{
-echo "<script>alert('Logo has Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
-}
-else
-{
-$image=md5($image).time().$extension;
- move_uploaded_file($_FILES["image"]["tmp_name"],"images/".$image);
-$sql="insert into tblstudent(StudentName,StudentEmail,StudentClass,Gender,DOB,StuID,FatherName,MotherName,ContactNumber,AltenateNumber,Address,UserName,Password,Image)values(:stuname,:stuemail,:stuclass,:gender,:dob,:stuid,:fname,:mname,:connum,:altconnum,:address,:uname,:password,:image)";
+
+$sql="insert into tblstudent(StudentName,StudentEmail,Gender,ContactNumber,UserName,Password)values(:stuname,:stuemail,:gender,:connum,:uname,:password)";
 $query=$dbh->prepare($sql);
 $query->bindParam(':stuname',$stuname,PDO::PARAM_STR);
 $query->bindParam(':stuemail',$stuemail,PDO::PARAM_STR);
-$query->bindParam(':stuclass',$stuclass,PDO::PARAM_STR);
+
 $query->bindParam(':gender',$gender,PDO::PARAM_STR);
-$query->bindParam(':dob',$dob,PDO::PARAM_STR);
-$query->bindParam(':stuid',$stuid,PDO::PARAM_STR);
-$query->bindParam(':fname',$fname,PDO::PARAM_STR);
-$query->bindParam(':mname',$mname,PDO::PARAM_STR);
+
 $query->bindParam(':connum',$connum,PDO::PARAM_STR);
-$query->bindParam(':altconnum',$altconnum,PDO::PARAM_STR);
-$query->bindParam(':address',$address,PDO::PARAM_STR);
+
 $query->bindParam(':uname',$uname,PDO::PARAM_STR);
 $query->bindParam(':password',$password,PDO::PARAM_STR);
-$query->bindParam(':image',$image,PDO::PARAM_STR);
  $query->execute();
    $LastInsertId=$dbh->lastInsertId();
    if ($LastInsertId>0) {
-    echo '<script>alert("Student has been added.")</script>';
-echo "<script>window.location.href ='add-students.php'</script>";
+    echo '<script>alert("Teacher has been added.")</script>';
+echo "<script>window.location.href ='add-teacher.php'</script>";
   }
   else
     {
          echo '<script>alert("Something Went Wrong. Please try again")</script>';
     }
 }}
-
-else
-{
-
-echo "<script>alert('Username or Student Id  already exist. Please try again');</script>";
-}
-}
   ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -125,31 +97,14 @@ echo "<script>alert('Username or Student Id  already exist. Please try again');<
                     <form class="forms-sample" method="post" enctype="multipart/form-data">
                       
                       <div class="form-group">
-                        <label for="exampleInputName1">Student Name</label>
+                        <label for="exampleInputName1">Teacher Name</label>
                         <input type="text" name="stuname" value="" class="form-control" required='true'>
                       </div>
                       <div class="form-group">
-                        <label for="exampleInputName1">Student Email</label>
+                        <label for="exampleInputName1">Teacher Email</label>
                         <input type="text" name="stuemail" value="" class="form-control" required='true'>
                       </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail3">Student Class</label>
-                        <select  name="stuclass" class="form-control" required='true'>
-                          <option value="">Select Class</option>
-                         <?php 
-
-$sql2 = "SELECT * from    tblclass ";
-$query2 = $dbh -> prepare($sql2);
-$query2->execute();
-$result2=$query2->fetchAll(PDO::FETCH_OBJ);
-
-foreach($result2 as $row1)
-{          
-    ?>  
-<option value="<?php echo htmlentities($row1->ID);?>"><?php echo htmlentities($row1->ClassName);?> <?php echo htmlentities($row1->Section);?></option>
- <?php } ?> 
-                        </select>
-                      </div>
+                  
                       <div class="form-group">
                         <label for="exampleInputName1">Gender</label>
                         <select name="gender" value="" class="form-control" required='true'>
@@ -158,40 +113,17 @@ foreach($result2 as $row1)
                           <option value="Female">Female</option>
                         </select>
                       </div>
-                      <div class="form-group">
-                        <label for="exampleInputName1">Date of Birth</label>
-                        <input type="date" name="dob" value="" class="form-control" required='true'>
-                      </div>
                      
-                      <div class="form-group">
-                        <label for="exampleInputName1">Student ID</label>
-                        <input type="text" name="stuid" value="" class="form-control" required='true'>
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputName1">Student Photo</label>
-                        <input type="file" name="image" value="" class="form-control" required='true'>
-                      </div>
-                      <h3>Parents/Guardian's details</h3>
-                      <div class="form-group">
-                        <label for="exampleInputName1">Father's Name</label>
-                        <input type="text" name="fname" value="" class="form-control" required='true'>
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputName1">Mother's Name</label>
-                        <input type="text" name="mname" value="" class="form-control" required='true'>
-                      </div>
+                     
+                      
+                    
+                    
                       <div class="form-group">
                         <label for="exampleInputName1">Contact Number</label>
                         <input type="text" name="connum" value="" class="form-control" required='true' maxlength="10" pattern="[0-9]+">
                       </div>
-                      <div class="form-group">
-                        <label for="exampleInputName1">Alternate Contact Number</label>
-                        <input type="text" name="altconnum" value="" class="form-control" required='true' maxlength="10" pattern="[0-9]+">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputName1">Address</label>
-                        <textarea name="address" class="form-control" required='true'></textarea>
-                      </div>
+                      
+                     
 <h3>Login details</h3>
 <div class="form-group">
                         <label for="exampleInputName1">User Name</label>
@@ -235,4 +167,4 @@ foreach($result2 as $row1)
     <script src="js/select2.js"></script>
     <!-- End custom js for this page -->
   </body>
-</html><?php }  ?>
+</html>
--- admin/manage-teacher.php
+++ admin/manage-teacher.php
@@ -14,7 +14,7 @@ $query=$dbh->prepare($sql);
 $query->bindParam(':rid',$rid,PDO::PARAM_STR);
 $query->execute();
  echo "<script>alert('Data deleted');</script>"; 
-  echo "<script>window.location.href = 'manage-students.php'</script>";     
+  echo "<script>window.location.href = 'manage-teacher.php'</script>";     
 
 
 }
@@ -23,7 +23,7 @@ $query->execute();
 <html lang="en">
   <head>
    
-    <title>Student  Management System|||Manage Students</title>
+    <title>MIET BR ADMIN |||Manage Students</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
     <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
@@ -32,6 +32,7 @@ $query->execute();
     <!-- Plugin css for this page -->
     <link rel="stylesheet" href="./vendors/daterangepicker/daterangepicker.css">
     <link rel="stylesheet" href="./vendors/chartist/chartist.min.css">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <!-- endinject -->
@@ -52,11 +53,11 @@ $query->execute();
         <div class="main-panel">
           <div class="content-wrapper">
              <div class="page-header">
-              <h3 class="page-title"> Manage Students </h3>
+              <h3 class="page-title"> Manage Teachers </h3>
               <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                   <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                  <li class="breadcrumb-item active" aria-current="page"> Manage Students</li>
+                  <li class="breadcrumb-item active" aria-current="page"> Manage Teachers</li>
                 </ol>
               </nav>
             </div>
@@ -65,19 +66,19 @@ $query->execute();
                 <div class="card">
                   <div class="card-body">
                     <div class="d-sm-flex align-items-center mb-4">
-                      <h4 class="card-title mb-sm-0">Manage Students</h4>
-                      <a href="#" class="text-dark ml-auto mb-3 mb-sm-0"> View all Students</a>
+                      <h4 class="card-title mb-sm-0">Manage Teachers</h4>
+                      <a href="#" class="text-dark ml-auto mb-3 mb-sm-0"> View all Teachers</a>
                     </div>
                     <div class="table-responsive border rounded p-1">
                       <table class="table">
                         <thead>
                           <tr>
-                            <th class="font-weight-bold">S.No</th>
-                            <th class="font-weight-bold">Student ID</th>
-                            <th class="font-weight-bold">Student Class</th>
-                            <th class="font-weight-bold">Student Name</th>
-                            <th class="font-weight-bold">Student Email</th>
-                            <th class="font-weight-bold">Admissin Date</th>
+                            <th class="font-weight-bold">T.No</th>
+                            <th class="font-weight-bold">Teacher Name</th>
+                            <th class="font-weight-bold">Teacher Email</th>
+                            <th class="font-weight-bold">Gender</th>
+                            <th class="font-weight-bold">Contact Number</th>
+                            <th class="font-weight-bold">Date Added</th>
                             <th class="font-weight-bold">Action</th>
                             
                           </tr>
@@ -98,7 +99,7 @@ $query1->execute();
 $results1=$query1->fetchAll(PDO::FETCH_OBJ);
 $total_rows=$query1->rowCount();
 $total_pages = ceil($total_rows / $no_of_records_per_page);
-$sql="SELECT tblstudent.StuID,tblstudent.ID as sid,tblstudent.StudentName,tblstudent.StudentEmail,tblstudent.DateofAdmission,tblclass.ClassName,tblclass.Section from tblstudent join tblclass on tblclass.ID=tblstudent.StudentClass LIMIT $offset, $no_of_records_per_page";
+$sql="SELECT tblstudent.StuID,tblstudent.StudentName,tblstudent.StudentEmail,tblstudent.Gender,tblstudent.ContactNumber,tblstudent.DateofAdded,tblclass.ClassName,tblclass.Section from tblstudent join tblclass on tblclass.ID=tblstudent.StudentClass LIMIT $offset, $no_of_records_per_page";
 $query = $dbh -> prepare($sql);
 $query->execute();
 $results=$query->fetchAll(PDO::FETCH_OBJ);
@@ -110,15 +111,16 @@ foreach($results as $row)
 {               ?>   
                           <tr>
                            
-                            <td><?php echo htmlentities($cnt);?></td>
+                            <td><?php  echo htmlentities($cnt);?></td>
                             <td><?php  echo htmlentities($row->StuID);?></td>
-                            <td><?php  echo htmlentities($row->ClassName);?> <?php  echo htmlentities($row->Section);?></td>
                             <td><?php  echo htmlentities($row->StudentName);?></td>
                             <td><?php  echo htmlentities($row->StudentEmail);?></td>
-                            <td><?php  echo htmlentities($row->DateofAdmission);?></td>
+                            <td><?php  echo htmlentities($row->Gender);?></td>
+                            <td><?php  echo htmlentities($row->ContactNumber);?></td>
+                            <td><?php  echo htmlentities($row->DateofAdded);?></td>
                             <td>
                               <div><a href="edit-student-detail.php?editid=<?php echo htmlentities ($row->sid);?>"><i class="icon-eye"></i></a>
-                                                || <a href="manage-students.php?delid=<?php echo ($row->sid);?>" onclick="return confirm('Do you really want to Delete ?');"> <i class="icon-trash"></i></a></div>
+                                                || <a href="manage-teacher.php?delid=<?php echo ($row->sid);?>" onclick="return confirm('Do you really want to Delete ?');"> <i class="icon-trash"></i></a></div>
                             </td> 
                           </tr><?php $cnt=$cnt+1;}} ?>
                         </tbody>
